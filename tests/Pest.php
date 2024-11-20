91b4c37160e120dc5e8b9283deb16974d9524d4a
Rik Meijer <hello@rikmeijer.nl>
2024-11-20 13:47:59 +0100

    fix

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,58 +1,6 @@
 <?php
 namespace {
 
+    require_once __DIR__ . '/Functions.php';
     nostriphant\FunctionalAlternate\extend_pest('expect');
-}
-
-/*
-|--------------------------------------------------------------------------
-| Functions
-|--------------------------------------------------------------------------
-|
-| While Pest is very powerful out-of-the-box, you may have some testing code specific to your
-| project that you don't want to repeat in every file. Here you can also expose helpers as
-| global functions to help you to reduce the number of lines of code in your test files.
-|
-*/
-namespace Pest {
-
-    use nostriphant\NIP01\Event;
-
-    function vectors(string $name): object {
-        return json_decode(file_get_contents(__DIR__ . '/vectors/' . $name . '.json'), false);
-    }
-
-
-    function key(string $nsec): Key {
-        return Key::fromHex(Bech32::fromNsec($nsec));
-    }
-
-    function key_sender(): Key {
-        return key('nsec15udyzkfk7twhpdmhu5syc4lqm7dxmll0jxeu0rq65f89gaewx0ps89derx');
-    }
-
-    function pubkey_sender(): string {
-        return key_sender()(Key::public());
-    }
-
-    function key_recipient(): Key {
-        return key('nsec1dm444kv7gug4ge7sjms8c8ym3dqhdz44x3jhq0mcq9eqftw9krxqymj9qk');
-    }
-
-    function pubkey_recipient(): string {
-        return key_recipient()(Key::public());
-    }
-
-    function event(array $event): Event {
-        return new Event(...array_merge([
-                    'id' => '',
-                    'pubkey' => '',
-                    'created_at' => time(),
-                    'kind' => 1,
-                    'content' => 'Hello World',
-                    'sig' => '',
-                    'tags' => []
-                        ], $event));
-    }
-
 }
